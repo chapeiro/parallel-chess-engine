d1123a14d89b147309e5e3531f1e50f0a05990ef
Chapeiro <periklis93@yahoo.gr>
2014-07-07 10:20:46 +0300

    Changed number of threads and tasks
    For a remote test
    
    Signed-off-by: Chapeiro <periklis93@yahoo.gr>

--- src/Parallel/ThreadBoardInterface.hpp
+++ src/Parallel/ThreadBoardInterface.hpp
@@ -15,10 +15,10 @@
 // #include "../BoardInterface/BoardInterface.hpp"
 #include "../BoardInterface/BareBoardInterface.hpp"
 
-constexpr unsigned int thread_pop(8);
+constexpr unsigned int thread_pop(2);
 constexpr unsigned int UI_index(thread_pop+1);
 constexpr unsigned int MASTER_index(thread_pop);
-constexpr unsigned int task_pop(16); // per thread
+constexpr unsigned int task_pop(8); // per thread
 constexpr unsigned int thrd_id_offset(16);
 constexpr unsigned int thr_task_mask((1 << task_pop) - 1);
 
